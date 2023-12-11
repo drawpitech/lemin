618483e1ad6fa742a94d9c67e9350798609bfdfa
Clément <clement2104.boillot@gmail.com>
2023-12-11 23:11:10 +0100

    Add function to process stdin

--- src/lemin.c
+++ src/lemin.c
@@ -5,9 +5,24 @@
 ** lemin
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "my/io.h"
+
 #include "lemin.h"
 
 int lemin(void)
 {
-    return 0;
+    size_t size = 0;
+    char *buffer = NULL;
+
+    while (getline(&buffer, &size, stdin) != -1) {
+        buffer[size] = '\0';
+        my_printf("%s", buffer);
+    }
+    if (buffer != NULL)
+        free(buffer);
+    return EXIT_SUCCESS;
 }
