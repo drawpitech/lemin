9c313588645c4941de9bf5c0f873a44bf2bfe488
Clément <clement2104.boillot@gmail.com>
2023-12-16 17:42:29 +0100

    Fix test_generator

---
+++ tests/test.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** lemin
+** File description:
+** test
+*/
+
+#ifndef TEST_H_
+    #define TEST_H_
+
+const char USAGE[] = (
+    "Usage:\n"
+    "\tunit_tests size density nb\n"
+    "Parameters:\n"
+    "\tsize:\t\tthe number of rooms.\n"
+    "\tdensity:\tthe percentage of probability of connexions between rooms.\n"
+    "\tnb:\t\tnumber of fourmiz.\n"
+);
+
+#endif
--- tests/test_generator.c
+++ tests/test_generator.c
@@ -5,13 +5,15 @@
 ** test_generator
 */
 
-#include "../src/lemin.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "test.h"
 
 static
 void print_rooms(long size)
 {
+    const int coef = 10;
     long home = rand() % size;
     long end = rand() % size;
 
@@ -21,28 +23,38 @@ void print_rooms(long size)
             printf("##start\n");
         if (i == end)
             printf("##end\n");
-        printf("%ld %ld %ld\n", i, rand() % (10 * size), rand() % (10 * size));
+        printf("%ld %ld %ld\n",
+            i,
+            rand() % (coef * size),
+            rand() % (coef * size));
     }
 }
 
 static
 void print_tunnels(long size, long density)
 {
+    const int coef = 100;
+
     for (long x = 0; x < (size * size); x++)
-        if (density > rand() % 100)
+        if (density > rand() % coef)
             printf("%ld-%ld\n", x / size, x % size);
 }
 
 int main(int argc, char **argv)
 {
     const int base_ten = 10;
-    long size = strtol(argv[1], NULL, base_ten);
-    long density = strtol(argv[2], NULL, base_ten);
-    long nb = strtol(argv[3], NULL, base_ten);
+    long density;
+    long size;
+    long nb;
 
     srand(0);
-    if (argc != 4)
-        return EXIT_FAILURE;
+    if (argc != 4) {
+        printf("%s", USAGE);
+        return EXIT_SUCCESS;
+    }
+    size = strtol(argv[1], NULL, base_ten);
+    density = strtol(argv[2], NULL, base_ten);
+    nb = strtol(argv[3], NULL, base_ten);
     printf("%ld\n", nb);
     print_rooms(size);
     print_tunnels(size, density);
